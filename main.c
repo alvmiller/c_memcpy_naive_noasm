268cb8fc795bf8e10baeffcadcbaa98d595a6ef5
alvmiller <134068320+alvmiller@users.noreply.github.com>
2025-05-01 10:24:32 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void memcpy_naive(void *restrict dst, const void *restrict src, size_t size)
+void *memcpy_naive(void *restrict dst, const void *restrict src, size_t size)
 {
     const size_t BASIC_SIZE = sizeof(size_t);
 
+    if ((dst == NULL || src == NULL) && size != 0) {
+        printf("\tError: Bad args\n");
+        return NULL;
+    }
+
+    if (size == 0 || dst == NULL || src == NULL) {
+        return dst;
+    }
+    
     if ((size_t)dst % BASIC_SIZE != 0 || (size_t)src % BASIC_SIZE != 0) {
         printf("\tError: Not aligned\n");
-        exit(-1);
+        return NULL;
     }
+
     size_t tmp = 0;
     if (__builtin_add_overflow((size_t)src, size, &tmp)) {
         printf("\tError: Overflow\n");
-        exit(-1);
+        return NULL;
     }
     if (__builtin_add_overflow((size_t)dst, size, &tmp)) {
         printf("\tError: Overflow\n");
-        exit(-1);
+        return NULL;
     }
 
     size_t *wdst = dst;
@@ -31,7 +41,7 @@ void memcpy_naive(void *restrict dst, const void *restrict src, size_t size)
         *(cdst++) = *(csrc++);
     }
 
-    return;
+    return dst;
 }
 
 int main()
@@ -40,11 +50,13 @@ int main()
         { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     size_t *dst = aligned_alloc(sizeof(size_t), 10 * sizeof *dst);
 
-    memcpy_naive(dst, src, 10 * sizeof(size_t));
-    for(size_t i = 0; i < 10; ++i) {
-        printf("%zu ", dst[i]);
+    void *ret = memcpy_naive(dst, src, 10 * sizeof(size_t));
+    if (ret != NULL) {
+        for(size_t i = 0; i < 10; ++i) {
+            printf("%zu ", dst[i]);
+        }
+        printf("\n");
     }
-    printf("\n");
 
     free (dst);
     return 0;
